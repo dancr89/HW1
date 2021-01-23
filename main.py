2583d931c521a945424e62e9e8658fa73e348cc5
Dan Craciun <dancraciun@Dans-MacBook-Air.local>
2021-01-23 17:56:21 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+my_list = [7,8,9,2,3,1,4,10,5,6]
+# print(my_list)
+my_list.sort()
+print(my_list)
+
+my_list.sort(reverse = True)
+print(my_list)
+
+my_list.sort()
+my_sliced_list = my_list[1::2]
+my_sliced_list.sort()
+print(my_sliced_list)
+
+my_list.sort()
+my_sliced_list = my_list[::2]
+my_sliced_list.sort()
+print(my_sliced_list)
+
+my_list.sort()
+my_sliced_list = my_list[2::3]
+my_sliced_list.sort()
+print(my_sliced_list)
+
+
+
